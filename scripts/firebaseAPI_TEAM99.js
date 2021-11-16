e1b222c07501c564bd5293b4b1151af2ecdc46c2
Colin Lam <colin.lph.lam@gmail.com>
2021-11-16 12:28:48 -0800

    finished demo11

---
+++ scripts/firebaseAPI_TEAM99.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDqunF4lnyZOu9ID9jS3KPJbgDKlbD_XXU",
+  authDomain: "demo8-comp1800-3e930.firebaseapp.com",
+  projectId: "demo8-comp1800-3e930",
+  storageBucket: "demo8-comp1800-3e930.appspot.com",
+  messagingSenderId: "287496196511",
+  appId: "1:287496196511:web:612406ccf2573e1b752daa",
+  measurementId: "G-MXQLWCGSBR"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
